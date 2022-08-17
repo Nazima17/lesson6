d4c66a0b036b4167e91aad9e77fe51488faca6bc
Nazima17 <egemberdinaz17@mail.ru>
2022-08-17 16:30:59 +0600

    ahahaha

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,22 @@ public class Main {
         boss.setDamage(240);
         boss.getWeapon().setWeaponType("Огнестрельные");
         System.out.println(boss.info());
+        System.out.println("________________________");
+        Skilleton skilleton1=new Skilleton();
+        Skilleton skilleton2=new Skilleton();
+        skilleton1.setDamage(33);
+        skilleton1.setHealth(300);
+        skilleton1.setUmberOfArrows(333);
+        skilleton1.getWeapon().setWeapon(WeaponType.GTA5);
+        skilleton1.getWeapon().setWeaponType("огнестрелное ");
+        skilleton2.setDamage(13);
+        skilleton2.setHealth(290);
+        skilleton2.setUmberOfArrows(111);
+        skilleton2.getWeapon().setWeapon(WeaponType.SHOTGUN);
+        skilleton2.getWeapon().setWeaponType("огнестрелное :");
+
+        System.out.println(skilleton1.info());
+        System.out.println("___________________________________");
+        System.out.println(skilleton2.info());
     }
 }
